3cf861204cca1c866aa9e8bd99428815c85513dc
Mathias Stadler <stadler-mathias@web.de>
2025-09-07 20:21:35 +0200

    save

--- scripts/10_gnome-terminal_bash_end_break.sh
+++ scripts/10_gnome-terminal_bash_end_break.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # a={wmctrl -l|tail -n 1 |tail -n 1 | awk '{print $1;}' }
-a=$(wmctrl -l|tail -n 1 |tail -n 1 | awk '{print $1;}')
-echo "$a";
-gnome-terminal -- bash -c "ls && a=$(wmctrl -l|tail -n 1 |tail -n 1 | awk '{print $1;}') && \\
- echo $a && \\
- wmctrl -i -r $a -e 0,0,0,600,800 && \\
- bash"
+# a=$(wmctrl -l|tail -n 1 |tail -n 1 | awk '{print $1;}')
+# echo "$a";
+# gnome-terminal -- bash -c "ls && a=$(wmctrl -l|tail -n 1 |tail -n 1 | awk '{print $1;}') && \\
+# echo $a && \\
+# wmctrl -i -r $a -e 0,0,0,600,800 && \\
+# bash"
 
+###
+gnome-terminal -- bash -c "a=$(wmctrl -l|tail -n 1 |tail -n 1 | awk '{print $1;}') && \\
+wmctrl -i -r $(xdotool getactivewindow) -e 0,0,0,600,800 && exit \\
+bash"
 #
